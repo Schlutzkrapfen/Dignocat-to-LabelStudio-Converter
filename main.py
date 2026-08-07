2e3e03069920b381003041e4e0ea546909925e76
Schlutzkrapfen <david.messner@protonmail.com>
2026-08-07 09:53:06 +0200

    changed some things back because of problems

--- main.py
+++ main.py
@@ -145,12 +145,12 @@ async def main():
                     if refrence_image_path == "":
                         print("Refrence Image is none")
                         continue
-                    images_paths = await get_user_screenshoots(page, user_id)
-                    task.append( await make_json(
-                        images_paths, label_Data, refrence_image_path, inner_task, user_id, page
-                    ))
+                images_paths = await get_user_screenshoots(page, user_id)
+                task.append( await make_json(
+                    images_paths, label_Data, refrence_image_path, inner_task, user_id, page
+                ))
 
-                    dump_json(task)
+            dump_json(task)
         finally:
             pass
 
